e1843cb789922e8226e2963fae12bd8a39a277cf
phpilatso <phenyopilatso@momentum.co.za>
2020-11-16 20:49:51 +0200

    Add Route to list customers. closes #2

--- src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
+++ src/main/java/com/sfg/gateway/config/LocalHostRouteConfig.java
@@ -13,6 +13,8 @@ public class LocalHostRouteConfig {
         return builder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
                         .uri("http://localhost:8080"))
+                .route(r -> r.path("/api/v1/customers/**")
+                        .uri("http://localhost:8081"))
                 .build();
     }
 }
